79dfe829ad71f14983222627663eeb169f6c0d21
Alessandro Gorla <ale.gorla2002@gmail.com>
2026-03-30 17:24:52 +0200

    feat(core): add component manifest and constants

---
+++ custom_components/focus_mode/const.py
@@ -0,0 +1,10 @@
+"""Constants for the Focus Mode integration."""
+
+from datetime import timedelta
+
+DOMAIN = "focus_mode"
+
+CONF_HOST = "host"
+CONF_TOKEN = "token"
+
+DEFAULT_POLLING_INTERVAL = timedelta(seconds=30)
